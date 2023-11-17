bba71689a249d77e584f304074bb679d50230073
sahdasamier <sahdasamier013@gmail.com>
2023-11-17 10:31:27 +0200

    apiroutes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 require('dotenv').config()
 
-
 const express = require('express')
-
+const workoutRoutes  = require('./routes/workouts')
 
 // express app
 const app = express()
 
 
 // middleware
-
+app.use(express.json())
 
 app.use((req, res, next) => { //you have to add next to be abe to move to the next peace in the middleware
   console.log(req.path, req.method)
@@ -18,12 +17,10 @@ app.use((req, res, next) => { //you have to add next to be abe to move to the ne
 
 
 // routes
-app.get('/', (req, res) => {
-   res.json({mssg: 'Welcome to the app'})
- })
+app.use('/api/workouts', workoutRoutes) 
 
 
  // listen for requests
- app.listen(process.env.PORT, () => {
+ app.listen(process.env.PORT, () => { //that mean it depends on the port in the env file
    console.log('listening on port', process.env.PORT)
  })
