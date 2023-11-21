bb9405903f014820173fdb791225415a25cd4d7d
sahdasamier <sahdasamier013@gmail.com>
2023-11-21 14:48:15 +0200

    mongo-atles

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes  = require('./routes/workouts')
 
 // express app
@@ -19,8 +20,17 @@ app.use((req, res, next) => { //you have to add next to be abe to move to the ne
 // routes
 app.use('/api/workouts', workoutRoutes) //the path you should to check it or change it in post man 
 
-
- // listen for requests
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+   // listen for requests
  app.listen(process.env.PORT, () => { //that mean it depends on the port in the env file
-   console.log('listening on port', process.env.PORT)
- })
+  console.log('listening on port', process.env.PORT)
+})
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
+
+ 
